95a982e14f09b8cedf3aaf08b6d0948580e5759f
Alex Francisco <alex2007f@gmail.com>
2020-12-04 20:53:57 -0300

    praticando

---
+++ ex06.js
@@ -0,0 +1,54 @@
+/* Por qual motivo o código abaixo retorna com erros?
+{
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+  }
+  console.log(var, marca, portas); R: Pelo fato de ter usado a palavra-chave var no console.log*/
+
+  
+  /* Como corrigir o erro abaixo?
+function somarDois(x) {
+    const dois = 2;
+    return x + dois;
+  }
+  function dividirDois(x) {
+    return x + dois;
+  }
+  somarDois(4);
+  dividirDois(6);*/
+
+  // Resposta
+  const dois = 2;
+
+  function somarDois(x) {
+    return x + dois;
+  }
+  function dividirDois(x) {
+    return x / dois;
+  }
+  console.log(somarDois(4));
+  console.log(dividirDois(6));
+  //Fim resposta
+  
+
+  /* O que fazer para total retornar 500?
+var numero = 50;
+
+for(var numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);*/
+
+  // Resposta
+  var numero = 50;
+  
+  for(let numero = 0; numero < 10; numero++) {
+    console.log(numero);
+  }
+  
+  const total = 10 * numero;
+  console.log(total);
+  //Fim resposta
