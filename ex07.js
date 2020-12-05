3a9b343c41c18778249ddb9d146d87838b79c90d
Alex Francisco <alex2007f@gmail.com>
2020-12-05 20:12:13 -0300

    praticando

--- ex07.js
+++ ex07.js
@@ -1,9 +1,14 @@
-<script src="ex06.js"></script>
 // Retorne o url da página atual utilizando o objeto window
+let url = document.location.href;
+console.log(url);
 
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
 
+let selecionaAtivo = document.querySelector('.ativo');
+console.log(selecionaAtivo)
+
 // Retorne a linguagem do navegador
 
 // Retorne a largura da janela 
+console.log(window.innerWidth)
