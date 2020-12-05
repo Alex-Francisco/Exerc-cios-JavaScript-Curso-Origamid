a735e237eb34a9da6f5b0d89b9077768ba8face5
Alex Francisco <alex2007f@gmail.com>
2020-12-05 19:57:08 -0300

    praticando

---
+++ ex07.js
@@ -0,0 +1,9 @@
+<script src="ex06.js"></script>
+// Retorne o url da página atual utilizando o objeto window
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela 
