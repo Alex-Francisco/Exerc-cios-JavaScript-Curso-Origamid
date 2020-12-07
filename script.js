2547b4c0a47de29b27370fcc8645493fd0140cd8
Alex Francisco <alex2007f@gmail.com>
2020-12-06 21:06:42 -0300

    praticando...

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ console.log(document.getElementsByTagName('img'));
 console.log(document.querySelectorAll('img'));
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imagensAnimais);
 
 // Selecione todos os links internos (onde o href começa com #)
-console.log(document.querySelectorAll('[href^="#"]'))
+console.log(document.querySelectorAll('[href^="#"]'));
 
 // Selecione o primeiro h2 dentro de .animais-descricao
-console.log(document.querySelector('.animais-descricao h2'))
+console.log(document.querySelector('.animais-descricao h2'));
 
 // Selecione o último p do site
 console.log(document.querySelector('p:last-child'))
