f16a9acaaeffb74f6c5832de79a1fe3b8d5f1022
Alex Francisco <alex2007f@gmail.com>
2020-12-08 21:21:33 -0300

    praticando...

--- ex10.js
+++ ex10.js
@@ -5,6 +5,16 @@ console.log(`A distância da primeira imagem em relação ao top é de ${primeir
 
 // Retorne a soma da largura de todas as imagens
 
+function larguraTotalImgs() {
+    const imagens = document.querySelectorAll('img');
+    let somaLarguraImgs = 0;
+    imagens.forEach((imagem) => {
+        somaLarguraImgs += imagem.offsetWidth;
+});
+    console.log(somaLarguraImgs)
+}
+
+
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
