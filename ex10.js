044b665823d197796f3c99ce6392ef72618984d4
Alex Francisco <alex2007f@gmail.com>
2020-12-08 21:40:55 -0300

    praticando...

--- ex10.js
+++ ex10.js
@@ -11,13 +11,25 @@ function larguraTotalImgs() {
     imagens.forEach((imagem) => {
         somaLarguraImgs += imagem.offsetWidth;
 });
-    console.log(somaLarguraImgs)
+    console.log(`A soma da largura de todas as imagens é de ${somaLarguraImgs} pixels`);
 }
-
 
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a');
+let larguraLink = 0;
+let alturaLink = 0;
+
+links.forEach((link) => {
+    larguraLink = link.offsetWidth;
+    alturaLink = link.offsetHeight;
+    if (larguraLink && alturaLink >= 48) {
+        console.log(`${link} está de acordo com o google`);
+    } else {
+        console.log(`${link} não está de acordo com o google`);
+    }
+});
 
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
