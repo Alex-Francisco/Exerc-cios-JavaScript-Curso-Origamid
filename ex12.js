6b6f538eede5d5d1546d6c307312c8696d560b6f
Alex Francisco <alex2007f@gmail.com>
2020-12-12 14:49:31 -0300

    praticando...

---
+++ ex12.js
@@ -0,0 +1,13 @@
+// Duplique o menu e adicione ele em copy
+
+const menu = document.querySelector('.menu');
+const cloneMenu = menu.cloneNode(true);
+const copy = document.querySelector('.copy');
+
+copy.appendChild(cloneMenu);
+
+// Selecione o primeiro DT da dl de Faq
+
+// Selecione o DD referente ao primeiro DT
+
+// Substitua o conteúdo html de .faq pelo de .animais
