0801d8ac73f803c45460728344046892da3b3cc4
Alex Francisco <alex2007f@gmail.com>
2020-12-11 21:51:39 -0300

    praticando...

--- ex11.js
+++ ex11.js
@@ -6,13 +6,16 @@
 const linksInternos = document.querySelectorAll('a[href^="#"');
 console.log(linksInternos);
 
-linksInternos.forEach((linkInterno) => {
-    linkInterno.addEventListener('click', () => {
-        linkInterno.preventDefault()
-        linkInterno.classList.toggle('ativo')
+function handleLink(event) {
+    event.preventDefault();
+    linksInternos.forEach((link) => {
+        link.classList.remove('ativo');
     });
-});
-
+    event.currentTarget.classList.add('ativo');
+}
+linksInternos.forEach((link) => {
+    link.addEventListener('click', handleLink);
+})
 
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
@@ -24,7 +27,7 @@ selecionaElemento.forEach((elemento) => {
         elemento.currentTarget
     });
 });
-
+/*
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
 const selecionaElementos = document.querySelectorAll('body *');
@@ -35,7 +38,7 @@ selecionaElementos.forEach((elemento) => {
         elemento.remove()
     });
 });
-
+*/
 
 // Se o usuário clicar na tecla (t), aumente todo o texto do site. 
 window.addEventListener('keydown', (pressionado) => {
