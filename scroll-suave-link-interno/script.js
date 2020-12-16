efb03b1d14fa3c2b95843b30340a50e16805b50c
Alex Francisco <alex2007f@gmail.com>
2020-12-16 12:47:19 -0300

    praticando...

--- scroll-suave-link-interno/script.js
+++ scroll-suave-link-interno/script.js
@@ -46,4 +46,22 @@ function initAccordion() {
 
 initAccordion();
 
-const linksInternos = document.querySelectorAll('.js-menu a[')
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');;
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+  
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+  });
+}
+
+initScrollSuave();
