ff17ab3c4b5a7f28c3c2073eee49dcb6d7ac737c
Alex Francisco <alex2007f@gmail.com>
2020-12-16 20:05:50 -0300

    praticando...

--- objetos/script.js
+++ objetos/script.js
@@ -1,13 +1,20 @@
-function Carro() {
-    this.marca = 'Marca';
-    this.preco = 2000;
+function Carro(marcaAtribuida, precoAtribuido) {
+    this.marca = marcaAtribuida;
+    this.preco = precoAtribuido;
 }
 
-const honda = new Carro();
-const fiat = new Carro();
-
-honda.marca = 'Honda';
-fiat.marca = 'Fiat';
+const honda = new Carro('Honda', 3000);
+const fiat = new Carro('Fiat', 4000);
 
 console.log(honda);
 console.log(fiat);
+
+function Carro2(marca, precoInicial) {
+    const taxa = 1.2;
+    const precoFinal = precoInicial * taxa;
+    this.marca = marca;
+    this.preco = precoFinal;
+}
+
+const mazda = new Carro2('Mazda', 58000)
+console.log(mazda)
