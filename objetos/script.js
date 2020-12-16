4856476d063eb51b3054ec7dbbb04371237d29ee
Alex Francisco <alex2007f@gmail.com>
2020-12-16 19:54:48 -0300

    praticando...

--- objetos/script.js
+++ objetos/script.js
@@ -1,5 +1,13 @@
 function Carro() {
-
+    this.marca = 'Marca';
+    this.preco = 2000;
 }
 
 const honda = new Carro();
+const fiat = new Carro();
+
+honda.marca = 'Honda';
+fiat.marca = 'Fiat';
+
+console.log(honda);
+console.log(fiat);
