c000d88a0a622b964c90114781cc417af23cd0dc
Alex Francisco <alex2007f@gmail.com>
2020-12-19 16:00:14 -0300

    praticando...

--- constructor-prototype/ex14.js
+++ constructor-prototype/ex14.js
@@ -22,16 +22,34 @@ console.log(alex.nomeCompleto());
 // dados criados com NodeList,
 // HTMLCollection, Document
 
+console.log(Object.getOwnPropertyNames(NodeList.prototype));
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
+console.log(Object.getOwnPropertyNames(Document.prototype));
+
 // Liste os construtores dos dados abaixo
 const li = document.querySelector('li');
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
+li; // HTMLLIElement
+console.log(li.constructor.name);
+
+li.click; // Function
+console.log(li.click.constructor.name);
+
+li.innerText; // String
+console.log(li.innerText.constructor.name);
+
+li.value; // Number
+console.log(li.value.constructor.name);
+
+li.hidden; // Boolean
+console.log(li.hidden.constructor.name);
+
+li.offsetLeft; // Number
+console.log(li.offsetLeft.constructor.name);
+
 li.click();
+console.log(li.click().constructor.name);
+
 
 // Qual o construtor do dado abaixo:
 li.hidden.constructor.name;
