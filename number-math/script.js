a2d17b8c32b61e48c52d2e239a810a81d232759a
Alex Francisco <alex2007f@gmail.com>
2020-12-22 19:51:26 -0300

    praticando...

---
+++ number-math/script.js
@@ -0,0 +1,16 @@
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN('Teste'));
+console.log(Number.isInteger(10));
+console.log(Number.isInteger(10.66544141));
+
+console.log(parseFloat('322255.58845'));
+console.log(parseFloat('322255,58845'));
+console.log(parseFloat('    322255.58845'));
+console.log(parseFloat('fasfasf322255.58845'));
+console.log(parseFloat('200 reais'));
+console.log(parseInt('23.78'));
+
+
+const 
+
+
