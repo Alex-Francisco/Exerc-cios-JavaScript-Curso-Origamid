628608b9d25d4778772a5aacf708a49004ad1350
Alex Francisco <alex2007f@gmail.com>
2020-12-21 22:35:17 -0300

    praticando...

--- native-host-user/script.js
+++ native-host-user/script.js
@@ -8,3 +8,15 @@
 
 // Liste 2 Métodos, Propriedades ou Objetos
 // presentes no Chrome que não existem no Firefox
+
+if (typeof document.webkitHidden !== 'undefined') {
+    console.log('Existe'); 
+} else {
+    console.log('Não Existe');
+}
+
+if (typeof document.webkitCancelFullScreen !== 'undefined') {
+    console.log('Existe'); 
+} else {
+    console.log('Não Existe');
+}
