402fd631bd711e04a32c3d4a986152fd64d94d39
Alex Francisco <alex2007f@gmail.com>
2020-12-21 21:45:52 -0300

    praticando...

---
+++ native-host-user/script.js
@@ -0,0 +1,10 @@
+// Liste 5 objetos nativos
+
+// -> Number, -> Boolean, -> String, -> Array e -> Function
+
+// Liste 5 objetos do browser
+
+//
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
