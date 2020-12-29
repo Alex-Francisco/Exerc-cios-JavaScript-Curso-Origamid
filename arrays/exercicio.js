a0ebb589c5cba82d0ff6922e85f08522d3acb85e
Alex Francisco <alex2007f@gmail.com>
2020-12-28 21:30:48 -0300

    praticando...

--- arrays/exercicio.js
+++ arrays/exercicio.js
@@ -1,16 +1,20 @@
 
 const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+
 // Remova o primeiro valor de comidas e coloque em uma variável
 const primeiroValor = comidas.shift();
 console.log(comidas);
 console.log(primeiroValor);
+
 // Remova o último valor de comidas e coloque em uma variável
 const ultimoValor = comidas.pop();
 console.log(comidas);
 console.log(ultimoValor);
+
 // Adicione 'Arroz' ao final da array
 comidas.push('Arroz');
 console.log(comidas);
+
 // Adicione 'Peixe' e 'Batata' ao início da array
 comidas.unshift('Peixe', 'Batata');
 console.log(comidas);
@@ -19,11 +23,18 @@ const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Arrume os estudantes em ordem alfabética
 estudantes.sort();
 console.log(estudantes);
+
 // Inverta a ordem dos estudantes
 estudantes.reverse();
 console.log(estudantes);
+
 // Verifique se Joana faz parte dos estudantes
-// Verifique se Juliana faz parte dos estudantes
+if (estudantes.includes('Joana')) {
+    console.log('O nome Joana está na lista');
+} else {
+    console.log('O nome Joana não está na lista');
+}
+
 
 let html = `<section>
               <div>Sobre</div>
@@ -32,6 +43,14 @@ let html = `<section>
             </section>`
 // Substitua section por ul e div com li,
 // utilizando split e join
+
+let htmlString = html;
+htmlString = htmlString.split('section');
+htmlString = htmlString.join('ul');
+
+htmlString = htmlString.split('div');
+htmlString = htmlString.join('li');
+console.log(htmlString);
 
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
 // Remova o último carro, mas antes de remover
