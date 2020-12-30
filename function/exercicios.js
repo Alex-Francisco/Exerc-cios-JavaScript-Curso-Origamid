e293dfe4a6f0d12bb4089261298536fbb8437691
Alex Francisco <alex2007f@gmail.com>
2020-12-30 10:17:08 -0300

    praticando...

--- function/exercicios.js
+++ function/exercicios.js
@@ -10,9 +10,30 @@ console.log(`O total de caracteres é igual a ${totalCaracteres}`)
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo) {
+    const elemento = document.createElement(tag);
+    elemento.classList.add(classe);
+    elemento.innerHTML = conteudo;
+    return elemento;
+}
 
+console.log(criarElemento('li', 'ativo', 'testando 123'));
 
 
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
+
+const criarH1 = criarElemento.bind(null, 'h1', 'titulo');
+console.log(criarH1('Aprendendo e praticando javascript'));
+console.log(criarH1('#praticar'));
+
+
+/*function criarH1(conteudo) {
+    const elemento = document.createElement('h1');
+    elemento.classList.add('titulo');
+    elemento.innerHTML = conteudo;
+    return elemento;
+}
+console.log(criarH1('elemento 1 criado com sucesso!'));
+console.log(criarH1('elemento 2 criado com sucesso!'));*/
