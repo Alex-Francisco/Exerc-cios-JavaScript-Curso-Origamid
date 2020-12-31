8bc790db13abb70d856104cc117252404dac2045
Alex Francisco <alex2007f@gmail.com>
2020-12-30 22:48:44 -0300

    praticando...

--- dataset/script.js
+++ dataset/script.js
@@ -1,6 +1,6 @@
 function iniTabNav() {
-  const tabMenu = document.querySelectorAll(".js-tabmenu li");
-  const tabContent = document.querySelectorAll(".js-tabContent section");
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
   if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add("ativo");
@@ -9,7 +9,8 @@ function iniTabNav() {
       tabContent.forEach((section) => {
         section.classList.remove("ativo");
       });
-      tabContent[index].classList.add("ativo");
+      const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add("ativo", direcao);
     }
 
     tabMenu.forEach((itemMenu, index) => {
@@ -24,7 +25,7 @@ iniTabNav();
 
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
   const activeClass = 'ativo';
  
 
@@ -47,7 +48,7 @@ function initAccordion() {
 initAccordion();
 
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
 
   function scrollToSection(event) {
     event.preventDefault();
@@ -66,7 +67,7 @@ function initScrollSuave() {
 
 initScrollSuave();
 
-const sections = document.querySelectorAll('.js-scroll');
+const sections = document.querySelectorAll('[data-anime="scroll"]');
 
 function initAnimacaoScroll(){
   if (sections.length) {
