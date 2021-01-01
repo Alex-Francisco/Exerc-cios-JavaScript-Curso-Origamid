dace107e6b6d2acc36b2b136e1569f1f57bd05a5
Alex Francisco <alex2007f@gmail.com>
2021-01-01 20:50:17 -0300

    praticando...

---
+++ modal/js/script-site.js
@@ -0,0 +1,11 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import iniTabNav from './modules/tab-nav.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+iniTabNav();
+
+
