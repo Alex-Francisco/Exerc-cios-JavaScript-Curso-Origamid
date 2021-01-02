2b40a9c7169fbd1f75815ca5dfd21e6b305f3781
Alex Francisco <alex2007f@gmail.com>
2021-01-02 09:26:07 -0300

    praticando...

--- tooltip/js/script-site.js
+++ tooltip/js/script-site.js
@@ -3,11 +3,13 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import iniTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 iniTabNav();
 initModal();
+initToolTip();
 
 
