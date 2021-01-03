175197e387563c6620d16cc04805eaf4e39c530d
Alex Francisco <alex2007f@gmail.com>
2021-01-03 19:25:03 -0300

    praticando...

--- button-creator/script.js
+++ button-creator/script.js
@@ -39,7 +39,23 @@ function handleChange(event) {
     const value = event.target.value;
 
     handleStyle[name](value);
+    saveValues(name, value);
     showCss();
+}
+
+function setValues() {
+    const properties = Object.keys(localStorage);
+    properties.forEach(propertie => {
+        handleStyle[propertie](localStorage[propertie])
+        controles.elements[propertie].value = localStorage[propertie];
+    })
+    console.log(properties)
+}
+
+setValues();
+
+function saveValues(name, value) {
+    localStorage[name] = value
 }
 
 function showCss() {
