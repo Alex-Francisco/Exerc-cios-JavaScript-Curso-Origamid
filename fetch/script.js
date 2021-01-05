68a9e86f5ffe1fefc4d896ae1903e8743fb55a61
Alex Francisco <alex2007f@gmail.com>
2021-01-05 19:28:06 -0300

    praticando...

---
+++ fetch/script.js
@@ -0,0 +1,28 @@
+/*const doc = fetch('./doc.txt');
+console.log(doc)
+
+doc.then(resolucao => {
+    return resolucao.text();
+}).then((body) => {
+    const conteudo = document.querySelector('.conteudo');
+    conteudo.innerText = body;
+})*/
+
+/*const cep = fetch('https://viacep.com.br/ws/09450000/json/');
+
+cep.then(r => r.json())
+.then(body => {
+    console.log(body.localidade);
+    const conteudo = document.querySelector('.conteudo');
+    conteudo.innerText = body.localidade;
+});*/
+
+const estilo = fetch('./style.css');
+
+estilo.then(r => r.text())
+.then(body => {
+    const conteudo = document.querySelector('.conteudo');
+    const criarElemento = document.createElement('style');
+    criarElemento.innerHTML = body;
+    conteudo.appendChild(criarElemento)
+});
