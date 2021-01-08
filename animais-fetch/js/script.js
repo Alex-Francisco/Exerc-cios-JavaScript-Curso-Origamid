e04a0469602c0f80a8e182c7b4dad37cc41bb725
Alex Francisco <alex2007f@gmail.com>
2021-01-08 20:56:45 -0300

    praticando...

--- animais-fetch/js/script.js
+++ animais-fetch/js/script.js
@@ -8,6 +8,8 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js'
 import initFetchAnimais from './modules/fetch-animais.js'
+import initFetchBitcoin from './modules/fetch-bitcoin.js'
+
 
 
 
@@ -21,3 +23,4 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
+initFetchBitcoin();
