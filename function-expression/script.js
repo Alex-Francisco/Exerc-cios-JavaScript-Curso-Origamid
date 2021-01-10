4d23a8900855fca751f343c6fbd95a179b1ef0f3
Alex Francisco <alex2007f@gmail.com>
2021-01-10 19:39:20 -0300

    praticando...

--- function-expression/script.js
+++ function-expression/script.js
@@ -8,3 +8,13 @@ const multiplicar = function(a, b) {
     return a * b;
 }
 console.log(multiplicar(3,4));
+
+
+
+const dividir = (a, b) => a / b;
+console.log(dividir(12, 2));
+
+
+
+const quadrado = a => a * a;
+console.log(quadrado(10));
