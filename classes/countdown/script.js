c95b3f5d8e7375b25e1e608f06253a9f23ef719b
Alex Francisco <alex2007f@gmail.com>
2021-01-10 19:23:26 -0300

    praticando...

--- classes/countdown/script.js
+++ classes/countdown/script.js
@@ -1,3 +1,9 @@
 import Countdown from './countdown.js';
 
 const tempoParaNatal = new Countdown('24 December 2021 23:59:59 GMT-0300');
+const tempoParaAnoNovo = new Countdown('31 December 2021 23:59:59 GMT-0300');
+
+console.log(tempoParaNatal.total);
+setInterval(() => {
+    console.log(tempoParaAnoNovo.total);
+}, 1000);
