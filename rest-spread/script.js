a36b14c4dc128c2e827813d95a8d91d37452bac8
Alex Francisco <alex2007f@gmail.com>
2021-01-13 10:19:31 -0300

    praticando...

--- rest-spread/script.js
+++ rest-spread/script.js
@@ -24,3 +24,19 @@ function perimetroForma(lado, totalLados = 4) {
 console.log(perimetroForma(10, 4));
 console.log(perimetroForma(10));
 console.log(perimetroForma(10, 2));
+
+function soma(a = 0, b = 0) {
+    console.log(arguments);
+    return a + b;
+}
+
+console.log(soma(10, 40));
+
+
+function mult(a = 0, b = 0) {
+    const argArray = Array.from(arguments);
+    console.log(argArray);
+    return a * b;
+}
+
+console.log(mult(10, 40));
